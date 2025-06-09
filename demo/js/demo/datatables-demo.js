fb19f449d936dd7845a705d1e780c26d83c5c776
geovana.stefani <geovana.stefani@enacom.com.br>
2025-06-09 12:27:36 +0100

    add demo

---
+++ demo/js/demo/datatables-demo.js
@@ -0,0 +1,7 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable({
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json'
+    });
+});
